02c6f36163de70c5ed266fb07b2b2cd70abbd619
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-26 17:56:31 +0530

    terraform_start

--- output.tf
+++ output.tf
@@ -2,14 +2,14 @@ output "vpc_sri_id" {
   value       = aws_vpc.main.id # see in notes
 }
 
-# output "public_subnet_ids" {
-#   value = aws_subnet.public[*].id
-# }
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
 
-# output "private_subnet_ids" {
-#   value = aws_subnet.private[*].id
-# }
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
 
-# output "database_subnet_ids" {
-#   value = aws_subnet.database[*].id
-# }
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
