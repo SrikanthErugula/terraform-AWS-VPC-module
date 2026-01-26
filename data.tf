708616c3cecae0e732afd18c25d15c5788d9c982
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-26 17:44:45 +0530

    terraform_start

--- data.tf
+++ data.tf
@@ -2,14 +2,29 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# data "aws_vpc" "default" { # for peering in peering.tf 
-#   default = true
-# }
+data "aws_vpc" "default" { # for peering in peering.tf 
+  default = true
+}
 
-# data "aws_route_table" "main" { # for peering in peering.tf
-#   vpc_id = data.aws_vpc.default.id
+
+# # Data source to find the default route table associated with the specific VPC
+
+# data "aws_route_table" "main" {
+#   vpc_id = data.aws_vpc.example.id
+
 #   filter {
 #     name   = "association.main"
 #     values = ["true"]
 #   }
 # }
+
+# the above is syntax for below code 
+
+
+data "aws_route_table" "main" { # for peering in peering.tf
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
--- peering.tf
+++ peering.tf
@@ -1,7 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_peering_connection
 # SYNTAX: 
 # resource "aws_vpc_peering_connection" "foo" {
-#   peer_owner_id = var.peer_owner_id #
+#   peer_owner_id = var.peer_owner_id # removed bcz same account 
 #   peer_vpc_id   = aws_vpc.bar.id
 #   vpc_id        = aws_vpc.foo.id
 
@@ -14,57 +14,59 @@
 #   }
 # }
 
-# resource "aws_vpc_peering_connection" "default" {
-#   count = var.is_peering_required ? 1 : 0 # this option so we need to use funtion type 
+resource "aws_vpc_peering_connection" "default" {
+  count = var.is_peering_required ? 1 : 0 # this option so we need to use condition type 
   
-#   peer_vpc_id   = data.aws_vpc.default.id # acceptor
-#   vpc_id        = aws_vpc.main.id
+  peer_vpc_id   = data.aws_vpc.default.id # acceptor
+  vpc_id        = aws_vpc.main.id # requester 
 
-#   accepter {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   requester {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   auto_accept   = true # for same account it will be work, if not it won't auto
+  auto_accept   = true # for same account it will be work, if not it won't auto
 
-#   tags = merge(
-#     var.vpc_tags,
-#     local.common_tags,
-#     {
-#         Name = "${local.common_name_suffix}-default"
-#     }
-#   )
+  tags = merge(
+    var.vpc_tags,
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-default"
+    }
+  )
 
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route
+# resource "aws_route" "r" {
+#   route_table_id            = aws_route_table.testing.id
+#   destination_cidr_block    = "10.0.1.0/22"
+#   vpc_peering_connection_id = "pcx-45ff3dc1"
 # }
 
-# # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route
-# # resource "aws_route" "r" {
-# #   route_table_id            = aws_route_table.testing.id
-# #   destination_cidr_block    = "10.0.1.0/22"
-# #   vpc_peering_connection_id = "pcx-45ff3dc1"
-# # }
+# so below code is for accepter peering connection in thier routes
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block # accepter cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
 
 
-# resource "aws_route" "public_peering" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = aws_route_table.public.id
-#   destination_cidr_block    = data.aws_vpc.default.cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
-# }
-
-# resource "aws_route" "private_peering" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = aws_route_table.private.id
-#   destination_cidr_block    = data.aws_vpc.default.cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
-# }
-
-# resource "aws_route" "default_peering" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = data.aws_route_table.main.id
-#   destination_cidr_block    = var.vpc_cidr
-#   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
-# }
+# so below code is for requester peering connection  in thier routes
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_route_table.main.id
+  destination_cidr_block    = var.vpc_cidr # requester cidr 
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
