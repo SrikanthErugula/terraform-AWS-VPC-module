dd5c23145d050aa1680bca8df4b20f32a9ae25c0
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-26 16:27:55 +0530

    terraform_start

--- data.tf
+++ data.tf
@@ -2,14 +2,14 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "aws_vpc" "default" { # for peering in peering.tf 
-  default = true
-}
+# data "aws_vpc" "default" { # for peering in peering.tf 
+#   default = true
+# }
 
-data "aws_route_table" "main" { # for peering in peering.tf
-  vpc_id = data.aws_vpc.default.id
-  filter {
-    name   = "association.main"
-    values = ["true"]
-  }
-}
+# data "aws_route_table" "main" { # for peering in peering.tf
+#   vpc_id = data.aws_vpc.default.id
+#   filter {
+#     name   = "association.main"
+#     values = ["true"]
+#   }
+# }
